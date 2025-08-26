684902aa56622cf884c03c68bd6338720aced76f
Alan Naranjo <125095298+AdamNrj@users.noreply.github.com>
2025-08-26 17:46:35 +0200

    feat: creating a project with its libraries.

---
+++ i18n/routing.ts
@@ -0,0 +1,14 @@
+import { createNavigation } from 'next-intl/navigation'
+import { defineRouting } from 'next-intl/routing'
+
+export const routing = defineRouting({
+  locales: ['en', 'es'] as const,
+  defaultLocale: 'es' as const,
+})
+
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing)
+
+export type Locale = (typeof routing.locales)[number]
